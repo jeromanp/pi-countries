7dec74bba00f4591812f2c3aec365446cfccd0df
jeromanp <jeduardo.roman69@gmail.com>
2023-01-30 14:09:04 -0600

    ingreso const PORT en listen server

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const PORT = 3001
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`); // eslint-disable-line no-console
   });
 });
