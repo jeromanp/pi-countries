0f0380651889a23ff68c3ddc2b644fc6927f2353
jeromanp <jeduardo.roman69@gmail.com>
2023-02-01 15:51:46 -0600

    Descripcion de rutas a implementar

--- api/src/routes/activityRouter.js
+++ api/src/routes/activityRouter.js
@@ -4,9 +4,25 @@ const {Router} = require("express")
 
 const activityRouter = Router()
 
-activityRouter.get("/",(req, res)=>{
-    res.send("Estoy en Activities");
+activityRouter.post("/",(req, res)=>{
+    res.send("Recibe los datos recolectados desde el formulario controlado de la ruta de creación de actividad turística por body. Crea una actividad turística en la base de datos, relacionada con los países correspondientes");
 })
+
+//EXTRA
+
+activityRouter.get("/",(req, res)=>{
+    res.send("Esta ruta me traera las actividades creadas");
+})
+
+activityRouter.put("/:id", (req, res)=>{
+    res.send("Modificar una actividad con el ID")
+})
+
+activityRouter.delete("/:id", (req, res)=>{
+    res.send("Eliminar una actividad con el ID")
+})
+
+
 
 
 module.exports = activityRouter
--- api/src/routes/countriesRouter.js
+++ api/src/routes/countriesRouter.js
@@ -6,8 +6,20 @@ const countriesRouter = Router()
 //const url =  "https://restcountries.com/v3/all"
 
 countriesRouter.get("/",(req,res)=>{
-    res.send("Estoy en Countries");
+    res.send("NIY: Esta ruta debera traer todos los paises desde la rest y guardarlos en mi BDD para luego utilizarlos desde ahi. ");
 })
+
+countriesRouter.get("/:idPais",(req,res)=>{
+    res.send("NIY: Obtener el detalle de un país en particular.Debe traer solo los datos pedidos en la ruta de detalle de país.Incluir los datos de las actividades turísticas correspondientes")
+})
+
+countriesRouter.get("/name=",(req, res)=>{
+    res.send("Obtener un listado de los paises.Obtener los países que coincidan con el nombre pasado como query parameter (No necesariamente tiene que ser una matcheo exacto). Si no existe ningún país mostrar un mensaje adecuado")
+})
+
+
+//EXTRA
+
 
 
 
