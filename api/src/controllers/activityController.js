42c773602c352e380a7b9ecfce197f8925d04691
jeromanp <jeduardo.roman69@gmail.com>
2023-02-01 23:04:55 -0600

    Modularizado de controllers

--- api/src/controllers/activityController.js
+++ api/src/controllers/activityController.js
@@ -1,24 +1,23 @@
+const createActivity = () => {
 
-const createActivity = (req, res) => {
-    //Recibe los datos recolectados desde el formulario controlado de la ruta de creación de actividad turística por body
-    //Crea una actividad turística en la base de datos, relacionada con los países correspondientes
 }
 
-const getActivity = (req, res) => {
-    //esta funcion traera todas las actividades creadas
+const getActivity = () => {
+
 }
 
-const updateActivity = (req, res) => {
-    //Modificara una determinada actividad pasada por ID
+const updateActivity = () => {
+
 }
 
-const deleteActivity = (req, res) => {
-    //Eliminar una actividad con el ID
+
+const deleteActivity = () => {
+
 }
 
 module.exports={
     createActivity,
     getActivity,
     updateActivity,
-    deleteActivity
+    deleteActivity,
 }
--- api/src/controllers/countriesController.js
+++ api/src/controllers/countriesController.js
@@ -1,42 +1,19 @@
-//const axios = require("axios")
-//const url =  "https://restcountries.com/v3/all"
-
 //estas funcion interactuan con el modelo de sequelize
 
-//para la siguiente funcion puedo emplearla para traer todas las countries y para buscarlas por el name, usando un condicional que pregunte si se tiene el params
-/*
-const {name} = req.params
-if(name){
-    res.send("Quiero buscar todos los que se llamen como name")
-}else{
-    res.send("quiero que me de todos los countries")
-}
-*/
-const getAllFroms = (req, res) => {
-    //esta funcion debera traer todos los paises desde la api-rest
-    //guardar los datos en la BDD para utilizarlos desde ahi
+//const axios = require("axios")
+//const url =  "https://restcountries.com/v3/all"
+const getAllFroms = () => {
+
 }
 
-const getFromById = (req, res) => {
-    //esta funcion debera obtener el detalle de un país en particular
-    //Debe traer solo los datos pedidos en la ruta de detalle de país.
-    //Incluir los datos de las actividades turísticas correspondientes
+const getFromById = () => {
+
 }
 
-// const filterFroms = (req, res) => {
-//     //esta funcion debera obtener un listado de los paises
-//     //Obtener los países que coincidan con el nombre pasado como query parameter (No necesariamente tiene que ser una matcheo exacto).
-//     //Si no existe ningún país mostrar un mensaje adecuado
-// }
 
-//EXTRA - Contemplar agregar la ruta
 
-//const createFrom = (res, res) => {}
 
-//const deleteFrom = (res, res) => {}
-
-module.exports = {
+module.exports={
     getAllFroms,
     getFromById,
-    //filterFroms,
 }
