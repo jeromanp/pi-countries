172b31e93738000334b61d821202fd01dfc287d2
jeromanp <jeduardo.roman69@gmail.com>
2023-02-01 19:07:01 -0600

    se agrega timestamps:false en los modelos

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,35 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Activity', {
-    id:{
-      type:DataTypes.INTEGER,
-      primaryKey:true,
-      autoIncrement:true,
+  sequelize.define(
+    "Activity",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          max: 5,
+          min: 1,
+        },
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: 1,
+        },
+      },
+      season: {
+        type: DataTypes.ENUM("Primavera", "Verano", "Otoño", "Invierno"),
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    difficulty:{
-      type:DataTypes.INTEGER,
-      allowNull:false,
-      validate:{
-        max:5,
-        min:1,
-      }
-    },
-    duration:{
-      type:DataTypes.INTEGER,
-      validate:{
-        min:1,
-      }
-    },
-    season:{
-      type:DataTypes.ENUM("Primavera","Verano", "Otoño", "Invierno" ),
-      allowNull:false,
-    },    
-  });
+    { timestamps: false }
+  );
 };
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,38 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Country', {
-    id:{
-      type:DataTypes.STRING(3),
-      primaryKey:true,
-      allowNull:false,
+  sequelize.define(
+    "Country",
+    {
+      id: {
+        type: DataTypes.STRING(3),
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      flag: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      continent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    flag:{
-      type:DataTypes.TEXT,
-      allowNull:false,
-    },
-    continent:{
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    capital:{
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    subregion:{
-      type:DataTypes.STRING,
-    },
-    area:{
-      type:DataTypes.INTEGER,
-    },
-    population:{
-      type:DataTypes.INTEGER,
-    }
-  });
+    { timestamps: false }
+  );
 };
