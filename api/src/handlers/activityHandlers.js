c07b9c3255b470014dc7a7e79bbb2587da287ef8
jeromanp <jeduardo.roman69@gmail.com>
2023-02-02 00:09:48 -0600

    createActivutyHAndler hecho

--- api/src/handlers/activityHandlers.js
+++ api/src/handlers/activityHandlers.js
@@ -1,40 +1,40 @@
 const {
-    createActivity,
-    getActivity,
-    updateActivity,
-    deleteActivity,
-} = require("../controllers/activityController")
+  createActivity,
+  getActivity,
+  updateActivity,
+  deleteActivity,
+} = require("../controllers/activityController");
 
-
-const createActivityHandler = (req, res) => {
-    //Recibe los datos recolectados desde el formulario controlado de la ruta de creación de actividad turística por body
-    //Crea una actividad turística en la base de datos, relacionada con los países correspondientes
-    const { name, difficulty, duration, season} = req.body
-    const newActivity = createActivity( name, difficulty, duration, season)
-
-}
+const createActivityHandler = async (req, res) => {
+  //Recibe los datos recolectados desde el formulario controlado de la ruta de creación de actividad turística por body
+  //Crea una actividad turística en la base de datos, relacionada con los países correspondientes
+  try {
+    const { name, difficulty, duration, season } = req.body;
+    const newActivity = await createActivity(name, difficulty, duration, season);
+    res.status(201).json(newActivity)
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+};
 
 const getActivityHandler = (req, res) => {
-    //esta funcion traera todas las actividades creadas
-    res.send("llamara a controller getActivity")
-
-}
+  //esta funcion traera todas las actividades creadas
+  res.send("llamara a controller getActivity");
+};
 
 const updateActivityHandler = (req, res) => {
-    //Modificara una determinada actividad pasada por ID
-    res.send("llamara a controller updateActivity")
-
-}
+  //Modificara una determinada actividad pasada por ID
+  res.send("llamara a controller updateActivity");
+};
 
 const deleteActivityHandler = (req, res) => {
-    //Eliminar una actividad con el ID
-    res.send("llamara a controller deleteActivity")
+  //Eliminar una actividad con el ID
+  res.send("llamara a controller deleteActivity");
+};
 
-}
-
-module.exports={
-    createActivityHandler,
-    getActivityHandler,
-    updateActivityHandler,
-    deleteActivityHandler,
-}
+module.exports = {
+  createActivityHandler,
+  getActivityHandler,
+  updateActivityHandler,
+  deleteActivityHandler,
+};
