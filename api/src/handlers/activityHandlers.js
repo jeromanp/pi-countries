98c74d886896c287a33538f0f5c5bd594f2c7d97
jeromanp <jeduardo.roman69@gmail.com>
2023-02-01 23:11:23 -0600

    Elimino id de req.body

--- api/src/handlers/activityHandlers.js
+++ api/src/handlers/activityHandlers.js
@@ -9,8 +9,8 @@ const {
 const createActivityHandler = (req, res) => {
     //Recibe los datos recolectados desde el formulario controlado de la ruta de creación de actividad turística por body
     //Crea una actividad turística en la base de datos, relacionada con los países correspondientes
-    const {id, name, difficulty, duration, season} = req.body
-    const newActivity = createActivity(id, name, difficulty, duration, season)
+    const { name, difficulty, duration, season} = req.body
+    const newActivity = createActivity( name, difficulty, duration, season)
 
 }
 
