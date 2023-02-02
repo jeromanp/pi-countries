9832fba712b135878b1612c2b2acd91656e4a67c
jeromanp <jeduardo.roman69@gmail.com>
2023-02-02 00:08:23 -0600

    controller createActivity hecho

--- api/src/controllers/activityController.js
+++ api/src/controllers/activityController.js
@@ -1,8 +1,11 @@
 //estas funcion interactuan con el modelo de sequelize
-const {Activity} = require("../db")
+const { Activity } = require("../db")
 
-const createActivity = async () => {
-
+//me da una promesa y con el async-await indico que me espere a que se resuelva esa promesa
+//Activity.create es una promesa
+const createActivity = async (name, difficulty, duration, season) => {
+    const newActivity = await Activity.create({name, difficulty, duration, season})
+    return newActivity
 }
 
 const getActivity = () => {
