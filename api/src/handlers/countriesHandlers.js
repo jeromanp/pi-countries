3c951ad5fbd8699f83a4bbc45b4c8cab4e280907
jeromanp <jeduardo.roman69@gmail.com>
2023-02-02 23:12:35 -0600

    Handler getAllCountries hecho

--- api/src/handlers/countriesHandlers.js
+++ api/src/handlers/countriesHandlers.js
@@ -1,36 +1,40 @@
-
 const {
-    getAllFroms,
-    getFromById,
-} = require("../controllers/countriesController")
-
+  getAllCountries,
+  findCountryByName,
+  getCountryById,
+} = require("../controllers/countriesController");
 
 //para la siguiente funcion puedo emplearla para traer todas las countries y para buscarlas por el name, usando un condicional que pregunte si se tiene el params
 
-const getAllFromsHandler = (req, res) => {
-    //esta funcion debera traer todos los paises desde la api-rest
-    //guardar los datos en la BDD para utilizarlos desde ahi
-    const {name} = req.query
-    if(name){
-        res.send("Llamar a los paises que coincidan con el name")
-    }else{
-        res.send("llamara a controller getAllFroms")
+const getAllCountriesHandler = async (req, res) => {
+  //esta funcion debera traer todos los paises desde la api-rest
+  //guardar los datos en la BDD para utilizarlos desde ahi
+  const { name } = req.query;
+  let country;
+  try {
+    if (name) {
+      country = await findCountryByName(name);
+    } else {
+      country = await getAllCountries();
     }
-}
+    res.status(200).json(country);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-const getFromByIdHandler = (req, res) => {
-    //esta funcion debera obtener el detalle de un país en particular
-    //Debe traer solo los datos pedidos en la ruta de detalle de país.
-    //Incluir los datos de las actividades turísticas correspondientes
-    const {id} = req.params
-    res.send("llamara a controller getFromById")
-}
-
-// const filterFroms = (req, res) => {
-//     //esta funcion debera obtener un listado de los paises
-//     //Obtener los países que coincidan con el nombre pasado como query parameter (No necesariamente tiene que ser una matcheo exacto).
-//     //Si no existe ningún país mostrar un mensaje adecuado
-// }
+const getCountryByIdHandler = async (req, res) => {
+  //esta funcion debera obtener el detalle de un país en particular
+  //Debe traer solo los datos pedidos en la ruta de detalle de país.
+  //Incluir los datos de las actividades turísticas correspondientes
+  const { id } = req.params;
+  try {
+    const findFromById = await getCountryById(id);
+    res.status(201).json(findFromById);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 //EXTRA - Contemplar agregar la ruta
 
@@ -39,7 +43,7 @@ const getFromByIdHandler = (req, res) => {
 //const deleteFrom = (res, res) => {}
 
 module.exports = {
-    getAllFromsHandler,
-    getFromByIdHandler,
-    //filterFroms,
-}
+  getAllCountriesHandler,
+  getCountryByIdHandler,
+  //filterFroms,
+};
