2a8682f781b1e5d6cdfa7b16562bad9fc158471d
jeromanp <jeduardo.roman69@gmail.com>
2023-02-03 14:07:40 -0600

    Se agregan comandos para consola postgres

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -32,11 +32,32 @@ module.exports = (sequelize) => {
       },
       area: {
         type: DataTypes.INTEGER,
+        get(){
+          const km = this.getDataValue("area");
+          return km ? `${km} km2` : null ;
+        },
       },
       population: {
         type: DataTypes.INTEGER,
+        get(){
+          const hab = this.getDataValue("population");
+          return hab ? `${hab} approximate population` : null ;
+        },
       },
     },
     { timestamps: false }
   );
 };
+
+
+/*
+CONSOLA POSTGRES
+\c countries
+
+select * from "Countries";
+ALTER TABLE "Countries" ALTER COLUMN nombre_columna TYPE varchar(255) USING name::varchar(255);
+
+columnas = {name,continet, capital, subregion}
+
+select * from "Countries";
+*/
