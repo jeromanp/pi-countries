6f896a14ac5aa75fe5e5932221af92dfdcb1cb61
jeromanp <jeduardo.roman69@gmail.com>
2023-02-03 19:54:38 -0600

    se agrega componente NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+//import "./App.css";
+import Home from "./components/Homes/Home";
+import Landindg from "./components/Landing/Landing";
+import Form from "./components/Form/Form";
+import Detail from "./components/Detail/Detail";
+import NavBar from "./components/NavBar/NavBar";
+import { Route, useLocation } from "react-router-dom";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      {pathname !== "/" && <NavBar />}
+      <Route exact path="/" component={Landindg} />
+      <Route path="/home" render={() => <Home />} />
+      <Route path="/detail" component={Detail} />
+      <Route path="/activities" component={Form} />
     </div>
   );
 }
