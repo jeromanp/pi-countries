5d1d8f6feef3a8b16c63850a3328b2d8f0f98b1f
jeromanp <jeduardo.roman69@gmail.com>
2023-02-03 23:10:10 -0600

    para que renderice CardsContainer

--- client/src/components/Homes/Home.jsx
+++ client/src/components/Homes/Home.jsx
@@ -1,7 +1,10 @@
+import CardsContainer from "../CardsContainer/CardsContainer"
+
 const Home = () => {
     return(
         <>
             <h1>Estoy en Home</h1>
+            <CardsContainer />
         </>
     )
 }
