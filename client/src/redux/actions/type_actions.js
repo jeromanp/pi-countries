6fde3ac84ee5ee07493623f92abb54b207bd0563
jeromanp <jeduardo.roman69@gmail.com>
2023-02-04 18:14:19 -0600

    action GET_COUNTRIES

---
+++ client/src/redux/actions/type_actions.js
@@ -0,0 +1,9 @@
+export const GET_COUNTRIES = "GET_COUNTRIES"
+
+
+
+// export const ADD_FAVORITE = "ADD_FAVORITE"
+// export const DELETE_FAVORITE = "DELETE_FAVORITE"
+// export const FILTER = "FILTER"
+// export const ORDER = "ORDER"
+// export const RESET = "RESET"
