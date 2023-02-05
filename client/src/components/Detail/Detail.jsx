adf7a2a3c86b72633b67794478df2d572204590d
jeromanp <jeduardo.roman69@gmail.com>
2023-02-05 12:16:43 -0600

    boton tohome

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -2,34 +2,43 @@ import React from "react";
 import style from "./Detail.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react"
+
+import { useEffect } from "react";
 import { getCountry } from "../../redux/actions/actions";
 
-
-
-const Detail = () => {
-  const {id} = useParams()
-  console.log(id);
+const Detail = (props) => {
+  const { id } = useParams();
   
-  const dispatch = useDispatch()
+
+  const dispatch = useDispatch();
   const country = useSelector((state) => state.country);
 
+  function backtoHome() {
+    return props.history.push("/home");
+  }
+
   useEffect(() => {
-    dispatch(getCountry(id))
-  },[dispatch,id])
+    dispatch(getCountry(id));
+  }, [dispatch, id]);
 
   return (
-    <div className={style.container}>   
-          <h1>Estoy en Detail</h1>  
-          {country && <img src={country.flag} alt={country.name + " flag"} />}
-          {country && <p>ID: {country.id}</p>}
-          {country && <p>Name={country.name} </p>}
-          {country && <p>Capital: {country.capital}</p>}
-          {country && <p>Subregion: {country.subregion} </p>}
-          {country && <p>Area: {country.area} </p>}
-          {country && <p>Population: {country.population} </p>}                     
+    <div className={style.container}>
+      <div>
+        <button onClick={backtoHome}>To Home</button>
+      </div>
+      <div>
+        
+      <h1>Estoy en Detail</h1>
+      {country && <img src={country.flag} alt={country.name + " flag"} />}
+      {country && <p>ID: {country.id}</p>}
+      {country && <p>Name={country.name} </p>}
+      {country && <p>Capital: {country.capital}</p>}
+      {country && <p>Subregion: {country.subregion} </p>}
+      {country && <p>Area: {country.area} </p>}
+      {country && <p>Population: {country.population} </p>}
+      </div>
     </div>
-  );;
+  );
 };
 
 export default Detail;
