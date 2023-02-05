2dd14788b99b2648defd2342f72d94af57ef3639
jeromanp <jeduardo.roman69@gmail.com>
2023-02-04 21:56:48 -0600

    estilos css

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,9 +3,9 @@ import style from "./Card.module.css"
 const Card = (props) => {
     return(
         <div className={style.card}>
+            <img src={props.flag} alt={props.name + " flag"} />
             <p>Name: {props.name}</p>
             <p>Continent: {props.continent}</p>
-            <img src={props.flag} alt={props.name + " flag"} />
         </div>
     )
 }
