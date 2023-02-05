8601378b3f0e500ecace57e2b73784aede8f6aee
jeromanp <jeduardo.roman69@gmail.com>
2023-02-04 18:16:50 -0600

    redneriza CardSContainer

--- client/src/components/Homes/Home.jsx
+++ client/src/components/Homes/Home.jsx
@@ -1,6 +1,15 @@
 import CardsContainer from "../CardsContainer/CardsContainer"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getCountries } from "../../redux/actions/actions"
 
 const Home = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getCountries())        
+    },[dispatch])
+
     return(
         <>
             <h1>Estoy en Home</h1>
