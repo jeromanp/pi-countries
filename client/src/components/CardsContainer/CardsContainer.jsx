c29c65c69e86f463b37f8b0614cac90f1707bd6c
jeromanp <jeduardo.roman69@gmail.com>
2023-02-04 23:01:50 -0600

    Acomodo de codigo

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,21 +1,21 @@
-import style from "./CardContainer.module.css"
+import style from "./CardContainer.module.css";
 import Card from "../Card/Card";
-import {  useSelector } from "react-redux";
-
+import { useSelector } from "react-redux";
 
 const CardsContainer = () => {
+  const countries = useSelector((state) => state.countries);
 
-const countries = useSelector(state=>state.countries)
-
-return (
+  return (
     <div className={style.container}>
       {countries.map((country) => {
-        return <Card 
-            key ={country.id}
-            name={country.name} 
+        return (
+          <Card
+            key={country.id}
+            name={country.name}
             continent={country.continent}
-            flag= {country.flag}                         
-        />;
+            flag={country.flag}
+          />
+        );
       })}
     </div>
   );
