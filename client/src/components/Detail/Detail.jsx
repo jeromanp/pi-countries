49ee53e2d1ae3c1201f66cd8be845d25a199ea39
jeromanp <jeduardo.roman69@gmail.com>
2023-02-05 01:49:52 -0600

    Componente detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,10 +1,35 @@
+import React from "react";
+import style from "./Detail.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react"
+import { getCountry } from "../../redux/actions/actions";
+
+
+
 const Detail = () => {
-    return(
-        <>
-            <h1>Estoy en Detail</h1>
-        </>
-    )
-}
+  const {id} = useParams()
+  console.log(id);
+  
+  const dispatch = useDispatch()
+  const country = useSelector((state) => state.country);
 
+  useEffect(() => {
+    dispatch(getCountry(id))
+  },[dispatch,id])
 
-export default Detail
+  return (
+    <div className={style.container}>   
+          <h1>Estoy en Detail</h1>  
+          {country && <img src={country.flag} alt={country.name + " flag"} />}
+          {country && <p>ID: {country.id}</p>}
+          {country && <p>Name={country.name} </p>}
+          {country && <p>Capital: {country.capital}</p>}
+          {country && <p>Subregion: {country.subregion} </p>}
+          {country && <p>Area: {country.area} </p>}
+          {country && <p>Population: {country.population} </p>}                     
+    </div>
+  );;
+};
+
+export default Detail;
