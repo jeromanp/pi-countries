4429a83dc690d5e19a41ba211b24daad2581ec31
jeromanp <jeduardo.roman69@gmail.com>
2023-02-05 14:57:21 -0600

    se renderiza componenete searchcountry

--- client/src/components/Homes/Home.jsx
+++ client/src/components/Homes/Home.jsx
@@ -2,17 +2,20 @@ import CardsContainer from "../CardsContainer/CardsContainer"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { getCountries } from "../../redux/actions/actions"
+import SearchCountry from "../SearchCountry/SearchCountry"
 
 const Home = () => {
     const dispatch = useDispatch()
-
+    
     useEffect(() => {
         dispatch(getCountries())        
     },[dispatch])
 
+
     return(
-        <>
+        <>            
             <h1>Estoy en Home</h1>
+            <SearchCountry />
             <CardsContainer />
         </>
     )
