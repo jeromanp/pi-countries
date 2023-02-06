aeb6e946cd0f2b438ba1444f9f62f4972f542200
jeromanp <jeduardo.roman69@gmail.com>
2023-02-05 20:23:12 -0600

    Select asc/desc countries

--- client/src/components/SearchCountry/SearchCountry.jsx
+++ client/src/components/SearchCountry/SearchCountry.jsx
@@ -1,27 +1,50 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { searchCountry } from "../../redux/actions/actions"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { searchCountry, orderCountries } from "../../redux/actions/actions";
 
 const SearchCountry = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-const [searchName, setSearchName] = useState("")
+  const [searchName, setSearchName] = useState("");
 
-
-const handleSearch = (e) => {
+  const handleSearch = (e) => {
     setSearchName(e.target.value);
   };
 
   const handleClick = () => {
     dispatch(searchCountry(searchName));
   };
-    return(
-        <div>
-            <h1>Buscando Country</h1>
-            <input type="text" value={searchName} onChange={handleSearch} />
-<button onClick={handleClick}>Search</button>
-        </div>
-    )
-}
 
-export default SearchCountry
+  function handleSelectOrder(event) {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    if (name === "Order") {
+      return dispatch(orderCountries(value));
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Buscando Country</h1>
+        <input type="text" value={searchName} onChange={handleSearch} />
+        <button onClick={handleClick}>Search</button>
+      </div>
+      <select
+        name="Order"
+        defaultValue={"Default"}
+        onChange={handleSelectOrder}
+      >
+        <option value="Default" disabled>
+          Select Order
+        </option>
+
+        <option value="Ascendente">Ascendente</option>
+        <option value="Descendente">Descendente</option>
+      </select>
+    </div>
+  );
+};
+
+export default SearchCountry;
