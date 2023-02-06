36f4ca711db6c08cb2e2ad8d0a1e1771820710f2
jeromanp <jeduardo.roman69@gmail.com>
2023-02-05 22:50:38 -0600

    Reset filters

--- client/src/redux/actions/type_actions.js
+++ client/src/redux/actions/type_actions.js
@@ -3,6 +3,7 @@ export const GET_COUNTRY = "GET_COUNTRY"
 export const SEARCH_NAME = "SEACH_NAME"
 export const ORDER_COUNTRIES = "ORDER_COUNTRIES"
 export const FILTER_CONTINENTS = "FILTER_CONTINENTS"
+export const RESET_FILTERS = "RESET_FILTERS"
 
 
 
