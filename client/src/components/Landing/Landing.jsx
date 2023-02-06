0c89d6fc4b54057210354d95d9e34c88a6b1b0bd
jeromanp <jeduardo.roman69@gmail.com>
2023-02-05 22:44:22 -0600

    boton toHome

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,7 +1,13 @@
-const Landindg = () => {
+const Landindg = (props) => {
+
+    function toHome(){
+        return props.history.push("/home")
+    }
+
     return(
         <>
             <h1>Estoy en Landindg</h1>
+            <button onClick={toHome}>Iniciar</button>
         </>
     )
 }
