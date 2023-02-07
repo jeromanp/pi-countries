999e81a92996ede93f67d2d0223064f83bc98eeb
jeromanp <jeduardo.roman69@gmail.com>
2023-02-07 00:14:12 -0600

    fn orderpopulation, resetstate

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,67 +1,78 @@
-import axios from "axios"
-import { GET_COUNTRIES, GET_COUNTRY, ORDER_COUNTRIES, SEARCH_NAME, FILTER_CONTINENTS, ORDER_ALPHABETICAL } from "./type_actions"
+import axios from "axios";
+import {
+  GET_COUNTRIES,
+  GET_COUNTRY,
+  SEARCH_NAME,
+  FILTER_CONTINENTS,
+  ORDER_ALPHABETIC,
+  RESET_STATE,
+  ORDER_POPULATION,
+} from "./type_actions";
 
-export function getCountries () {
-    return async function (dispatch){
-        const apiData = await axios.get("http://localhost:3001/countries")
-        //const apiData = await axios.get("https://jsonplaceholder.typicode.com/users")
-        //para ver si trabaja bien redux
-        const countries = apiData.data
-        dispatch({
-            type:GET_COUNTRIES,
-            payload:countries,
-        })
-    }    
+export function getCountries() {
+  return async function (dispatch) {
+    const apiData = await axios.get("http://localhost:3001/countries");
+    //const apiData = await axios.get("https://jsonplaceholder.typicode.com/users")
+    //para ver si trabaja bien redux
+    const countries = apiData.data;
+    dispatch({
+      type: GET_COUNTRIES,
+      payload: countries,
+    });
+  };
 }
 
-export function getCountry (id) {
-    return async function(dispatch){
-        const apiData = await axios.get(`http://localhost:3001/countries/${id}`)
-        const country = apiData.data
-        dispatch({
-            type:GET_COUNTRY,
-            payload:country
-        })
+export function getCountry(id) {
+  return async function (dispatch) {
+    const apiData = await axios.get(`http://localhost:3001/countries/${id}`);
+    const country = apiData.data;
+    dispatch({
+      type: GET_COUNTRY,
+      payload: country,
+    });
+  };
+}
+
+export function searchCountry(name) {
+  return async function (dispatch) {
+    try {
+      const apiData = await axios.get(
+        `http://localhost:3001/countries?name=${name}`
+      );
+      const search = apiData.data;
+      dispatch({
+        type: SEARCH_NAME,
+        payload: search,
+      });
+    } catch (error) {
+      alert(error.response.data.error);
     }
+  };
 }
 
-export function searchCountry (name){
-    return async function(dispatch){
-        try {
-            const apiData = await axios.get(`http://localhost:3001/countries?name=${name}`)
-            const search = apiData.data
-            dispatch({
-                type:SEARCH_NAME,
-                payload:search
-            })
-        } catch (error) {
-            alert(error.response.data.error)
-        }
-    }
+export function filterContinents(payload) {
+  return {
+    type: FILTER_CONTINENTS,
+    payload,
+  };
 }
 
-export function orderCountries (order) {
-    return {
-        type:ORDER_COUNTRIES,
-        payload:order
-    }
+export function resetState() {
+  return {
+    type: RESET_STATE,
+  };
 }
 
-export function filterContinent (continent){
-    return {
-        type: FILTER_CONTINENTS,
-        payload:continent
-    }
+export function orderAlphabetic(payload) {
+  return {
+    type: ORDER_ALPHABETIC,
+    payload,
+  };
 }
 
-export function orderAlpha (orderAlpha){
+export function orderPopulation (orderAlpha){
     return{
-        type:ORDER_ALPHABETICAL,
+        type:ORDER_POPULATION,
         payload:orderAlpha
     }
 }
-
-
-
-
-
