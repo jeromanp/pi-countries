50b8db42a7777fbd99d32fffed823415c6febeb4
jeromanp <jeduardo.roman69@gmail.com>
2023-02-07 11:41:10 -0600

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ const server = express();
 
 server.name = 'API';
 
-//server.use(cors())
+server.use(cors())
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
