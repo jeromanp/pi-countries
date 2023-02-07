fa16a2fec935006390b7defc9d216153d7c30500
jeromanp <jeduardo.roman69@gmail.com>
2023-02-07 16:27:26 -0600

    fn para traer datos y mostrar en DB

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,47 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require("axios");
+const { Country } = require("../db");
+
 const countriesRouter = require("./countriesRouter")
 const activityRouter = require("./activityRouter")
 
 const router = Router();
+
+const url = "https://restcountries.com/v3/all";
+
+//funcion que me mostrara que valores quiero en la bdd
+const showApiValues = (array) => {
+    return array.map((element) => {
+      return {
+        id: element.cca3,
+        name: element.name.common,
+        continent:element.continents,
+        capital: element.capital
+          ? element.capital[0]
+          : "Este pais no tiene capital.",
+        flag: element.flags[0],
+        continent: element.continents[0],
+        subregion: element.subregion,
+        area: element.area,
+        population: element.population,
+        created: false,
+      };
+    });
+  };
+
+let dataFetched = false;
+//se busca que se ejecute una sola vez
+const getDataApi = async () => {
+  if (!dataFetched) {
+    const dataApi = (await axios.get(url)).data;
+    await Country.bulkCreate(showApiValues(dataApi));
+    dataFetched = true;
+    return dataApi;
+  }
+};
+getDataApi()
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
