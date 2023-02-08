9944616fc64873ee6433871e4c56d3200015df10
jeromanp <jeduardo.roman69@gmail.com>
2023-02-08 17:38:02 -0600

    estilos

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,6 +8,8 @@ const Card = (props) => {
           <Link to={`/detail/${props.id}`}>
               {props.flag && <img src={props.flag} alt={props.name + " flag"} />}        
           </Link>
+
+          <div className={style.text}>            
         {props.id && <p>ID: {props.id}</p>}
         {props.name && <p>Name: {props.name}</p>}
         {props.continent && <p>Continent: {props.continent}</p>}
@@ -15,6 +17,7 @@ const Card = (props) => {
         {props.subregion && <p>Subregion: {props.subregion}</p>}
         {props.area && <p>Area: {props.area}</p>}
         {props.population && <p>Population: {props.population}</p>}
+          </div>
       </div>
     );
   };
