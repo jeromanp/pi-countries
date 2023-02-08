d4c5f3a4915a24dd327f89d7b90c9c0f808b7d89
jeromanp <jeduardo.roman69@gmail.com>
2023-02-08 14:49:15 -0600

    Toexit

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,11 +2,13 @@ import style from "./NavBar.module.css"
 import { Link } from "react-router-dom"
 
 const NavBar = () => {
+
     return (
         <div className={style.mainContainer}>
             <Link to="/home">Home</Link>
             <Link to="/activities">Crear Actividad</Link>
             <Link to="/about">About</Link>
+            <Link to="/">Exit</Link>
         </div>
     )
 }
