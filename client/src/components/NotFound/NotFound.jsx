a5307e1353d72a802acbf7789f4414ab3ca38441
jeromanp <jeduardo.roman69@gmail.com>
2023-02-08 23:41:03 -0600

    NotFound404 componenete

---
+++ client/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import style from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={style.container}>
+      <div>
+        <h1>Error 404! Not Found</h1>
+      </div>      
+    </div>
+  );
+};
+
+export default NotFound;
