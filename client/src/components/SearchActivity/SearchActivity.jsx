f96357c27f61eda798652ece38efce22bd6612e2
jeromanp <jeduardo.roman69@gmail.com>
2023-02-08 21:48:22 -0600

    estilos

--- client/src/components/SearchActivity/SearchActivity.jsx
+++ client/src/components/SearchActivity/SearchActivity.jsx
@@ -1,12 +1,12 @@
 import style from "./SearchActivity.module.css";
-import { useDispatch, /*useSelector*/ } from "react-redux";
+import { useDispatch /*useSelector*/ } from "react-redux";
 import { useState } from "react";
 import { filterByActivity, getAllActivity } from "../../redux/actions/actions";
 
 const SearchActivity = () => {
   const dispatch = useDispatch();
 
-//   const countries = useSelector((state) => state.countries);
+  //   const countries = useSelector((state) => state.countries);
   const [searchAct, setSearchAct] = useState("");
 
   const handleClickAll = () => {
@@ -14,27 +14,29 @@ const SearchActivity = () => {
   };
 
   const handleClick = () => {
-    dispatch(filterByActivity(searchAct));    
+    dispatch(filterByActivity(searchAct));
   };
 
   const handleSearchAct = (e) => {
     setSearchAct(e.target.value);
-  };  
+  };
 
   return (
     <div className={style.container}>
-      <h1>Buscando Activity</h1>
-        <div>
       <button onClick={handleClickAll}>Get All Activity</button>
-            
-        </div>
-      <input
-        type="text"
-        value={searchAct}
-        onChange={handleSearchAct}
-        placeholder="Name of Country"
-      />
-      <button onClick={handleClick}>Search Activity</button>
+      <h1>Search Activity</h1>
+
+      <div className={style.search}>
+        <input
+          type="text"
+          value={searchAct}
+          onChange={handleSearchAct}
+          placeholder="Activity"
+        />
+        <button className={style.search} onClick={handleClick}>
+          Search Activity
+        </button>
+      </div>
     </div>
   );
 };
