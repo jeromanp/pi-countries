bc4b34586ae807a5dd4501e424e078664aa01777
jeromanp <jeduardo.roman69@gmail.com>
2023-02-08 23:43:35 -0600

    condicional para componenete 404

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import style from "./App.module.css"
+import style from "./App.module.css";
 import Home from "./components/Homes/Home";
 import Landindg from "./components/Landing/Landing";
 import Form from "./components/Form/Form";
 import Detail from "./components/Detail/Detail";
 import NavBar from "./components/NavBar/NavBar";
 import About from "./components/About/About";
+import NotFound from "./components/NotFound/NotFound";
 import { Route, useLocation } from "react-router-dom";
 
 function App() {
@@ -12,12 +13,13 @@ function App() {
 
   return (
     <div className={style.container}>
-      {location.pathname !== "/" && <NavBar />}
+      {location.pathname !== "/" && location.pathname !== "/NotFound" && <NavBar />}
       <Route exact path="/" component={Landindg} />
       <Route path="/home" render={() => <Home />} />
-      <Route path="/detail/:id"  component={Detail} />
+      <Route path="/detail/:id" component={Detail} />
       <Route path="/activities" component={Form} />
       <Route path="/about" component={About} />
+      <Route component={NotFound} />
     </div>
   );
 }
