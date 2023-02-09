75c43c125d911712486fb32a20a378e91b709304
jeromanp <jeduardo.roman69@gmail.com>
2023-02-08 19:07:32 -0600

    estilos

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -5,10 +5,12 @@ const NavBar = () => {
 
     return (
         <div className={style.mainContainer}>
+            <nav className={style.navbar}>                
             <Link to="/home">Home</Link>
             <Link to="/activities">Crear Actividad</Link>
             <Link to="/about">About</Link>
             <Link to="/">Exit</Link>
+            </nav>
         </div>
     )
 }
