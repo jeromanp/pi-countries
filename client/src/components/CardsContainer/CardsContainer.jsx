98e2e1342cc0e4af46efea1587abbbcd44918b93
jeromanp <jeduardo.roman69@gmail.com>
2023-02-10 00:29:01 -0600

    Mejora

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,26 +1,13 @@
 import style from "./CardContainer.module.css";
-import Card from "../Card/Card";
-import { useSelector } from "react-redux";
+// import Card from "../Card/Card";
+// import { useSelector } from "react-redux";
 
 const CardsContainer = () => {
-  const countries = useSelector((state) => state.countries);
+//  const countries = useSelector((state) => state.countries);
 
   return (
     <div className={style.container}>
-      {countries.map((country) => {
-        return (
-          <Card
-            key={country.id}
-            id={country.id}
-            name={country.name}
-            continent={country.continent}
-            flag={country.flag}
-            season={country.season}
-            difficulty={country.difficulty}
-            duration={country.duration}
-          />
-        );
-      })}
+      <img src="" alt="" />
     </div>
   );
 };
