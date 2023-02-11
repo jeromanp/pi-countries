a66f4c2431b7993d29054763ab85ef8b949faa9a
jeromanp <jeduardo.roman69@gmail.com>
2023-02-10 20:13:43 -0600

    se quitan eleemtos

--- client/src/components/SearchActivity/SearchActivity.jsx
+++ client/src/components/SearchActivity/SearchActivity.jsx
@@ -1,11 +1,11 @@
 import style from "./SearchActivity.module.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, /*useSelector*/ } from "react-redux";
 import { useState } from "react";
 import { filterByActivity } from "../../redux/actions/actions";
 
 const SearchActivity = () => {
   const dispatch = useDispatch();
-  const countries = useSelector((state) => state.countries);
+  // const countries = useSelector((state) => state.countries);
   const [searchAct, setSearchAct] = useState("");
 
   const handleClick = () => {
