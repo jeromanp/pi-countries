570db418ab0f11b1ed1c4bd2a872b889b1a3585d
jeromanp <jeduardo.roman69@gmail.com>
2023-02-10 18:32:11 -0600

    Estilos css

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -4,7 +4,8 @@ const NotFound = () => {
   return (
     <div className={style.container}>
       <div>
-        <h1>Error 404! Not Found</h1>
+        <h1>Error 404!</h1>
+        <h2>Not Found</h2>
       </div>      
     </div>
   );
