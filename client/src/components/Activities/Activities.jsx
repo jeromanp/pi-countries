c5b402d0e4caf20be590a371428a0b56fd1b1d7b
jeromanp <jeduardo.roman69@gmail.com>
2023-02-10 19:17:36 -0600

    renderizado actividades

--- client/src/components/Activities/Activities.jsx
+++ client/src/components/Activities/Activities.jsx
@@ -1,10 +1,15 @@
 import style from "./Activities.module.css";
 import { useSelector } from "react-redux";
-import Card from "../Card/Card";
+import SearchActivity from "../SearchActivity/SearchActivity";
+import CardActivity from "../CardActivity/CardActivity";
+// import { resetState } from "../../redux/actions/actions";
+// import { useDispatch } from "react-redux";
 
 const Activities = (props) => {
   const countries = useSelector((state) => state.countries);
   console.log(countries);
+
+  //   const dispatch = useDispatch();
 
   function backtoHome() {
     return props.history.push("/home");
@@ -12,28 +17,31 @@ const Activities = (props) => {
 
   return (
     <div className={style.container}>
-        <div>
+      <div>
         <button onClick={backtoHome}>To Home</button>
       </div>
+      <SearchActivity />
       <h1>All Activities</h1>
-      <div>
+      <div className={style.cardActivity}>
         {countries?.map((c) => {
           return (
-            <Card
+            <CardActivity
               key={c.id}
-              id={c.id}
+              //   id={c.id}
               name={c.name}
               flag={c.flag}
               continent={c.continent}
               difficulty={c.difficulty}
               duration={c.duration}
               season={c.season}
+              countries={c.flag}
               src="https://img.freepik.com/vector-premium/diseno-viajes-turismo_24908-34483.jpg"
               alt="img"
             />
           );
         })}
       </div>
+
     </div>
   );
 };
