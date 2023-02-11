40fcf10111f94923868141e5b88e19c0eb7226b5
jeromanp <jeduardo.roman69@gmail.com>
2023-02-11 02:15:49 -0600

    cardAct

--- client/src/components/CardActivity/CardActivity.jsx
+++ client/src/components/CardActivity/CardActivity.jsx
@@ -9,9 +9,16 @@ const CardActivity = (props) => {
         {props.season && <p>Season: {props.season}</p>}
         {props.difficulty && <p>Dificulty: {props.difficulty}</p>}
         {props.duration && <p>Duration: {props.duration}</p>}
-        {props.countries && <p>Countries: {props.countries}</p>}
+        {props.activityId && <p>Act ID: {props.activityId} </p>}
+        {props.activityName && <p>Act Name: {props.activityName} </p>}
+        {props.countryId && <p>Country ID:{props.countryId}</p>}  
+        {props.countryName && <p>Country:{props.countryName}</p>} 
       </div>
-      <div className={style.icono}>{props.src && <img src={props.src} alt={props.name} />}</div>
+      <div className={style.content}>
+        {props.countryFlag && <img src={props.countryFlag} alt={props.countryName} />}       
+
+      </div>
+      {/* <div className={style.icono}>{props.src && <img src={props.src} alt={props.name} />}</div> */}
     </div>
   );
 };
