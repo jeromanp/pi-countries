f61e017180952fed97571e554069bc2a050105df
jeromanp <jeduardo.roman69@gmail.com>
2023-02-12 02:30:25 -0600

    condicional si hay propso

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,9 +6,9 @@ const Card = (props) => {
     <div className={style.card}>
       <div className={style.cardDetail}>
       {props.flag && <img src={props.flag} alt={props.name + " flag"} />}
-        <p className={style.textTitle}>{props.name}</p>
-        <p className={style.textBody}>Continent: {props.continent}</p>
-        {/* <p className={style.textBody}>{props.id}</p> */}
+        {props.name && <p className={style.textTitle}>{props.name}</p>}
+        {props.continent && <p className={style.textBody}>Continent: {props.continent}</p>}
+        {/* {props.id && <p className={style.textBody}>{props.id}</p>} */}
       </div>
       <Link to={`/detail/${props.id}`}>      
       <button className={style.cardButton}>More info</button>
