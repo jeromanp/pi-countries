63f85256f67aaa5576615a6313b3a752c4ce31f6
jeromanp <jeduardo.roman69@gmail.com>
2023-02-12 13:11:11 -0600

    estilos css componenete

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -12,3 +12,25 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
+// import { Link } from 'react-router-dom';
+
+// import style from './NotFound.module.css';
+
+// const NotFound = () => {
+//   return (
+//     <div className={style.container}>
+//       <div className={style.infoContainer}>
+//             <h4 className={style.title}>Error - 404</h4>
+//         <div className={style.containerAnimation}>
+//           <div className={`${style.globeLoader} fa-solid fa-globe`}>
+//             <i className="fas fa-plane"></i>
+//             <Link className={style.link} to='/home'>Home</Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default NotFound
