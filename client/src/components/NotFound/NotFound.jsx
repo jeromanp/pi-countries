90d1e793b8ba8f558a64f5bc1e1a21afeae9d0cc
jeromanp <jeduardo.roman69@gmail.com>
2023-02-12 23:04:50 -0600

    component 404

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,36 +1,20 @@
-import style from "./NotFound.module.css";
+import React from 'react'
+import style from "./NotFound.module.css"
+import { Link } from 'react-router-dom'
+
 
 const NotFound = () => {
   return (
-    <div className={style.container}>
-      <div>
-        <h1>Error 404!</h1>
-        <h2>Not Found</h2>
-      </div>      
+    <div>
+      <div className={style.container}>
+  <h2>Oops! Page not found.</h2>
+  <h1>404</h1>
+  <p>We can't find the page you're looking for.</p>
+  <Link to="/home">Go back home</Link>
+</div>
+
     </div>
-  );
-};
+  )
+}
 
-export default NotFound;
-
-// import { Link } from 'react-router-dom';
-
-// import style from './NotFound.module.css';
-
-// const NotFound = () => {
-//   return (
-//     <div className={style.container}>
-//       <div className={style.infoContainer}>
-//             <h4 className={style.title}>Error - 404</h4>
-//         <div className={style.containerAnimation}>
-//           <div className={`${style.globeLoader} fa-solid fa-globe`}>
-//             <i className="fas fa-plane"></i>
-//             <Link className={style.link} to='/home'>Home</Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default NotFound
+export default NotFound
