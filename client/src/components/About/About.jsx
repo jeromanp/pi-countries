bbe9a665353ca34da938d1a73396d28e6ea0c2e7
jeromanp <jeduardo.roman69@gmail.com>
2023-02-13 10:46:20 -0600

    css button

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -9,60 +9,63 @@ const About = (props) => {
       <div>
         <button onClick={backtoHome}>To Home</button>
       </div>
+
       <div className={style.info}>
-        <h1>Sobre el proyecto Countries</h1>
-        <p>
-          Proyecto Final Individual para aprobar el primer módulo de Labs del
-          Bootcamp de Henry.
-        </p>
-        <p>
-          Este pequeño proyecto cuenta con librerías como React, Redux para el
-          FronEnd; Express y Sequelize con Postgres para el BackEnd
-        </p>
-        <p>Contacto:</p>
-
-        <ul>
+        <div className={style.conta}>
+          <h1>Sobre el proyecto Countries</h1>
           <p>
-            <li>
-              Email:
-              <a href="mailto:jeduardo.roman69@gmail.com">
-                jeduardo.roman69@gmail.com
-              </a>
-            </li>
+            Proyecto Final Individual para aprobar el primer módulo de Labs del
+            Bootcamp de Henry.
           </p>
+          <p>
+            Este pequeño proyecto cuenta con librerías como React, Redux para el
+            FronEnd; Express y Sequelize con Postgres para el BackEnd
+          </p>
+          <p>Contacto:</p>
 
-          <li>
+          <ul>
             <p>
-              Linkedin
-              <a
-                href="https://www.linkedin.com/in/jose-eduardo-roman/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
-                  alt="LinkedIn Logo"
-                />
-              </a>
+              <li>
+                Email:
+                <a href="mailto:jeduardo.roman69@gmail.com">
+                  jeduardo.roman69@gmail.com
+                </a>
+              </li>
             </p>
-          </li>
 
-          <li>
-            <p>
-              Github
-              <a
-                href="https://github.com/jeromanp"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                  alt="Github Logo"
-                />
-              </a>
-            </p>
-          </li>
-        </ul>
+            <li>
+              <p>
+                Linkedin
+                <a
+                  href="https://www.linkedin.com/in/jose-eduardo-roman/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+                    alt="LinkedIn Logo"
+                  />
+                </a>
+              </p>
+            </li>
+
+            <li>
+              <p>
+                Github
+                <a
+                  href="https://github.com/jeromanp"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                    alt="Github Logo"
+                  />
+                </a>
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
