479c55676879fd659f6266b935d2f4153e4df235
jeromanp <jeduardo.roman69@gmail.com>
2023-02-15 17:25:22 -0600

    mejroas

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -10,6 +10,7 @@ const Landindg = (props) => {
         <div className={style.container}>
             <h1>P.I. COUNTRIES</h1>
             <h2>Click to start</h2>
+            <h4>By: Jose Eduardo Roman Piña</h4>
             <button onClick={toHome}>Go</button>
         </div>
     )
