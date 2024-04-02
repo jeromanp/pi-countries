fa01db72dd470b4a47523b51baa517424d7c121c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-04-01 19:16:28 -0600

    analityc from vercel

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,14 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux';
 import store from "./redux/store/store"
+import { Analytics } from '@vercel/analytics/react';
+
 
 ReactDOM.render(
   <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Analytics/>
       </BrowserRouter>
   </Provider>,
   document.getElementById('root')
