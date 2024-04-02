f2758d41c131a4b62be1aba2ab913ff36fdc0834
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-04-01 19:18:57 -0600

    fix analitics vercel

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,12 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux';
 import store from "./redux/store/store"
-import { Analytics } from '@vercel/analytics/react';
 
 
 ReactDOM.render(
   <Provider store={store}>
       <BrowserRouter>
         <App />
-        <Analytics/>
       </BrowserRouter>
   </Provider>,
   document.getElementById('root')
